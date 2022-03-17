41aa2996890dd16b5ec491db96cbe034f49a50f2
Student <Student@IIST.local>
2022-03-17 12:00:32 +0300

    Management

--- Management/Form1.cs
+++ Management/Form1.cs
@@ -15,12 +15,34 @@ namespace Management
         String[] imgList = new String[]
             {"1.jpeg", "2.jpg", "3.jpg", "1.jpeg", "2.jpg"};
         int index = 0;
+
+        Country[] countries = new Country[5];
         public Form1()
         {
             InitializeComponent();
             pictureBox2.Load("../../Pictures/" + imgList[0]);
             pictureBox3.Load("../../Pictures/" + imgList[1]);
             pictureBox4.Load("../../Pictures/" + imgList[2]);
+
+
+                Country country = new Country();
+
+                // 5 элементов
+                country.Set("ARG", "Argentina");
+                countries[0] = country;
+
+                country.Set("AT", "Austria");
+                countries[1] = country;
+
+                country.Set("AUS", "Australia");
+                countries[2] = country;
+
+                country.Set("BI", "Bosnia");
+                countries[3] = country;
+
+                country.Set("BIH", "Bosnia Herzegovina");
+                countries[4] = country;
+            
         }
 
         private void button3_Click(object sender, EventArgs e)
---
+++ Management/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Management
+{
+    internal class Player
+    {
+        String Playerld;
+        String lastName;
+        String firstName;
+        String gender;
+        float height;
+        float weidth;
+        String shirtNumber;
+        String email;
+        String countryCode;
+        int Teamld;
+
+    }
+}
